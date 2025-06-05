76cfd8ba74c26ea1e13166b138204133f1cd2bbc
Grant Harris <me@grantharris.tech>
2025-06-05 00:03:33 -0600

    Fix linting issues in migration files
    - Fixed whitespace issues (W293)
    - Fixed import sorting with isort
    - Applied black formatting
    
    All linting checks now pass ✅

--- alembic/versions/feda888bea0d_merge_migration_heads.py
+++ alembic/versions/feda888bea0d_merge_migration_heads.py
@@ -5,9 +5,9 @@ Revises: c3d2e1f4a5b6, add_data_quality_monitoring_models
 Create Date: 2025-01-06 12:00:00.000000
 
 """
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'feda888bea0d'
